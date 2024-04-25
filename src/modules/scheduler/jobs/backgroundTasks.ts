7c236224898cab45a04ecc499bf0cda98254422b
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-04-25 14:29:27 +0100

    chore: only local

--- src/modules/scheduler/jobs/backgroundTasks.ts
+++ src/modules/scheduler/jobs/backgroundTasks.ts
@@ -3,8 +3,8 @@ import AppConfig from '../../../config/config'
 import { GENERATE_COURSE_TRENDS, RESUME_TOMORROW } from '../MessageTypes'
 import { generateCurrentCourseTrends } from '../../courses/service.courses'
 import { CourseEnrollment } from '@/modules/webhooks/interfaces.webhooks'
-// import config from '../../../config/config'
-// import { sendResumptionMessage } from '../../webhooks/service.webhooks'
+import config from '../../../config/config'
+import { sendResumptionMessage } from '../../webhooks/service.webhooks'
 
 const handleCourseTrends: Processor<{ courseId: string, teamId: string }> = async (job: Job<{ courseId: string, teamId: string }>) => {
   try {
@@ -20,10 +20,10 @@ const handleCourseTrends: Processor<{ courseId: string, teamId: string }> = asyn
 
 const handleContinueTomorrow: Processor<{ enrollment: CourseEnrollment, phoneNumber: string, messageId: string }> = async (job: Job<{ enrollment: CourseEnrollment, phoneNumber: string, messageId: string }>) => {
   try {
-    if (AppConfig.env !== "test") {
-      // const data = job.attrs.data
-      // const { enrollment, phoneNumber } = data
-      // await sendResumptionMessage(phoneNumber, `${config.redisBaseKey}enrollments:${phoneNumber}:${enrollment.id}`, enrollment)
+    if (AppConfig.env === "local") {
+      const data = job.attrs.data
+      const { enrollment, phoneNumber } = data
+      await sendResumptionMessage(phoneNumber, `${config.redisBaseKey}enrollments:${phoneNumber}:${enrollment.id}`, enrollment)
     }
   } catch (error) {
     console.log(error, "error send message")
