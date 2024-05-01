23849bdc5c0a46cc8f6444ae5d8697369d58f33f
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-05-01 20:18:54 +0100

    chore: debug

--- src/modules/slack/interfaces.slack.ts
+++ src/modules/slack/interfaces.slack.ts
@@ -35,7 +35,7 @@ export enum SlackTextMessageTypes {
 export enum SlackActionType {
   BUTTON = "button",
   TEXTINPUT = "plain_text_input",
-  SELECT = "plain_static_select"
+  SELECT = "static_select"
 }
 
 export interface SlackTextMessage {
