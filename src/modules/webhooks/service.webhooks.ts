cb76a8e6fde8cf8d4c388ecfceb0292ee2fa822c
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-05-02 14:16:57 +0100

    chore: remove the new line replace

--- src/modules/webhooks/service.webhooks.ts
+++ src/modules/webhooks/service.webhooks.ts
@@ -69,7 +69,7 @@ export function convertToWhatsAppString (html: string, indent: number = 0): stri
   if (!html) return ''
   let formattedText = html
   formattedText = formattedText.replace(/\s+([^\s]+)="/gi, ' $1=')
-  formattedText = formattedText.replace(/\n/g, '')
+  // formattedText = formattedText.replace(/\n/g, '')
   // Replace <br /> tags with new lines
   formattedText = formattedText.replace(/<br\s*\/?>/gi, '\n')
   // formattedText = formattedText.replace(/<p(?:\s+[^>]*?)?>(.*?)<\/p>/gi, '')
