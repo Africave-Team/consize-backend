8ae096da0058beeedb477068f6c98a45c1ba1f25
Emmanuel Onoja <emmanuel@steadyinstall.com>
2024-06-17 09:30:59 +0100

    chore: fixed course list not showing in bundle intro

--- src/modules/webhooks/service.webhooks.ts
+++ src/modules/webhooks/service.webhooks.ts
@@ -720,7 +720,7 @@ export const startBundle = async (phoneNumber: string, courseId: string, student
   const key = `${config.redisBaseKey}enrollments:${phoneNumber}:${courseId}`
   const initialMessageId = v4()
   if (course && student) {
-    let courses = await Courses.find({ id: { $in: course.courses } })
+    let courses = await Courses.find({ _id: { $in: course.courses } })
     const settings = await Settings.findById(course.settings)
     if (redisClient.isReady) {
       let totalBlocks = 0
@@ -728,7 +728,7 @@ export const startBundle = async (phoneNumber: string, courseId: string, student
       const courseOwner = await Team.findById(course.owner)
       let payload: CourseFlowItem = {
         type: CourseFlowMessageType.INTRO,
-        content: `This is a bundle of courses. *Bundle title*: ${course.title}\n\n*Bundle description*: ${description}\n\n*Course Organizer*: ${courseOwner?.name}\n📓 Total courses in the bundle: ${course.courses.length}. \n\nYou will receive the following courses in this order\n\n\n${courses.map((r, index) => `${index + 1}. *${r.title}*`).join('\n')}. \n\nHappy learning.`
+        content: `This is a bundle of courses. \n*Bundle title*: ${course.title}\n\n*Bundle description*: ${description}\n\n*Course Organizer*: ${courseOwner?.name}\n📓 Total courses in the bundle: ${course.courses.length}. \n\nYou will receive the following courses in this order\n\n\n${courses.map((r, index) => `${index + 1}. *${r.title}*`).join('\n')}. \n\nHappy learning.`
       }
       if (course.headerMedia && course.headerMedia.url && course.headerMedia.url.startsWith('https://')) {
         payload.mediaType = course.headerMedia.mediaType
