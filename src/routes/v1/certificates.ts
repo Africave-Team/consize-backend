76aac247acf925eae5b79e5a8356c5282c9631af
theprinceofcodes <princejason109@gmail.com>
2024-07-14 22:17:00 +0100

    chore: certificate feature

---
+++ src/routes/v1/certificates.ts
@@ -0,0 +1,14 @@
+import express, { Router } from "express"
+import { validate } from '../../modules/validate'
+import { auth } from '../../modules/auth'
+import { certificatesControllers, certificatesValidator } from "../../modules/certificates/"
+
+
+const router: Router = express.Router()
+router.use(auth())
+
+router.post('/', validate(certificatesValidator.createCertificates), certificatesControllers.createCertificates)
+router.get('/', certificatesControllers.getCertificate)
+router.put('/:id', validate(certificatesValidator.updateCertificates), certificatesControllers.updateCertificate)
+
+export default router
