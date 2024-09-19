e04a19072aa7043706e371515c8317abf8e0e12b
theprinceofcodes <princejason109@gmail.com>
2024-09-19 06:39:50 +0100

    chore: fix for lessons count in workflow

--- src/modules/webhooks/service.webhooks.ts
+++ src/modules/webhooks/service.webhooks.ts
@@ -179,6 +179,7 @@ export const generateCourseFlow = async function (courseId: string) {
 
     // blocks
     let lessonIndex = 0
+    let lessonCount = 0
     for (let content of course.contents) {
       if (content.lesson) {
         let lessonData = await Lessons.findById(content.lesson)
@@ -187,7 +188,7 @@ export const generateCourseFlow = async function (courseId: string) {
           for (let blockId of lessonData.blocks) {
             let content = ``
             if (blockIndex === 0) {
-              content = `*Lesson ${lessonIndex + 1}: ${lessonData.title.trim()}*`
+              content = `*Lesson ${lessonCount + 1}: ${lessonData.title.trim()}*`
             }
             const blockData = await Blocks.findById(blockId)
             if (blockData) {
